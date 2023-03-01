4131329721bef55e0e7dbfdc1d3a2f89899ef4be
Rabia Qureshi <rabia.q123@gmail.com>
2023-03-01 15:27:02 -0500

    Placed current search functionality (for tags and memories) into one search bar Ref #38

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -28,8 +28,8 @@ const Home = () => {
 
   const searchPost = () => {
     if (search.trim() || tags) {
-      dispatch(getPostsBySearch({ search, tags: tags.join(',') }));
-      history.push(`/posts/search?searchQuery=${search || 'none'}&tags=${tags.join(',')}`);
+      dispatch(getPostsBySearch({ search, tags: search }));
+      history.push(`/posts/search?searchQuery=${search || 'none'}`);
     } else {
       history.push('/');
     }
@@ -41,10 +41,6 @@ const Home = () => {
     }
   };
 
-  const handleAddChip = (tag) => setTags([...tags, tag]);
-
-  const handleDeleteChip = (chipToDelete) => setTags(tags.filter((tag) => tag !== chipToDelete));
-
   return (
     <Grow in>
       <Container maxWidth="xl">
@@ -55,14 +51,6 @@ const Home = () => {
           <Grid item xs={12} sm={6} md={3}>
             <AppBar className={classes.appBarSearch} position="static" color="inherit">
               <TextField onKeyDown={handleKeyPress} name="search" variant="outlined" label="Search Memories" fullWidth value={search} onChange={(e) => setSearch(e.target.value)} />
-              <ChipInput
-                style={{ margin: '10px 0' }}
-                value={tags}
-                onAdd={(chip) => handleAddChip(chip)}
-                onDelete={(chip) => handleDeleteChip(chip)}
-                label="Search Tags"
-                variant="outlined"
-              />
               <Button onClick={searchPost} className={classes.searchButton} variant="contained" color="primary">Search</Button>
             </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
