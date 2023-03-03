8db54310105332484373c3e76facf37205adb024
Rabia Qureshi <rabia.q123@gmail.com>
2023-03-02 20:06:44 -0500

    Display user posts on the user's profile page Ref #40

--- client/src/components/ProfilePage/Profile.jsx
+++ client/src/components/ProfilePage/Profile.jsx
@@ -6,26 +6,22 @@ import Post from '../Posts/Post/Post';
 import { getPostsByCreator } from '../../actions/posts';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { getUser, getUsers } from '../../actions/user';
-import { getPosts, getPost } from '../../actions/posts';
+import { getUser } from '../../actions/user';
 
 const Profile = () => {
   const { name } = useParams(); // user's name from URL
-  // const { posts, isLoading } = useSelector((state) => state.posts);
-  
+
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.user);
-  const { post, posts, isLoading } = useSelector((state) => state.posts);
+  const { posts, isLoading } = useSelector((state) => state.posts);
 
-  console.log('user', user);
-  console.log('user_posts', posts)
-  // console.log("the user is:" + user);
-
+  // console.log('user', user);
+  // console.log('user_posts', posts)
 
   useEffect(() => {
-    dispatch(getUser(name)); // THIS IS THE PROBLEM LINE
+    dispatch(getUser(name));
     dispatch(getPostsByCreator(name));
-    console.log(name);
+    // console.log(name);
   }, [name]);
 
   return (
@@ -33,9 +29,7 @@ const Profile = () => {
       <Typography variant="h2">{name}</Typography>
       <Divider style={{ margin: '20px 0 50px 0' }} />
 
-      {/* <Typography variant="h4">{user?.email}</Typography> */}
-
-      {/* {isLoading ? <CircularProgress /> : (
+      {isLoading ? <CircularProgress /> : (
       <Grid container alignItems="stretch" spacing={3}>
         {posts?.map((post) => (
         <Grid key={post._id} item xs={12} sm={12} md={6} lg={3}>
@@ -44,7 +38,8 @@ const Profile = () => {
         ))}
       </Grid>
       )}
-      {!posts && <Typography variant="h2">No posts</Typography>} */}
+      {!posts && <Typography variant="h2">No posts</Typography>}
+
     </div>
   )
 }
