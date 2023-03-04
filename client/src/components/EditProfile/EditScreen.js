705b8d6b27ba499ec51c2dbac4228f863152f95f
Rabia Qureshi <rabia.q123@gmail.com>
2023-03-03 21:15:03 -0500

    Prevent access to /editprofile endpoint if user is not logged in Ref #42

--- client/src/components/EditProfile/EditScreen.js
+++ client/src/components/EditProfile/EditScreen.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Button, Typography, Paper, Container, Grid } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUser, updateUserProfile } from '../../actions/user';
+import { useHistory } from 'react-router-dom';
 
 import useStyles from './styles';
 import Input from '../Auth/Input';
@@ -12,9 +13,14 @@ const EditScreen = () => {
 
   const initialState = JSON.parse(localStorage.getItem('profile'));
   const id = initialState?.result?._id;
+  const history = useHistory();
   const { user } = useSelector((state) => state.user);
   const [name, setName] = useState(user?.name);
   const [email, setEmail] = useState(user?.email);
+
+  if (!initialState) { // if user is not logged in, redirect to login page
+    history.push('/auth');
+  }
 
   // This is a helper function that can be used for updating a users profile
   const update_user = (id, email, name) => {
@@ -44,25 +50,28 @@ const EditScreen = () => {
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <Paper className={classes.paper} elevation={6}>
-        <Typography component="h1" variant="h5" align='center' style={{marginBottom: '16px'}}>
-          Edit Profile
-        </Typography>
-        <form className={classes.form} onSubmit={handleSubmit}>
-          <Grid container spacing={2}>
-            <>
-              {/* TODO: split name into first and last name (by space) */}
-              <Input name="name" label="Name" handleChange={handleChange} autoFocus value={name}/>
-              <Input name="email" label="Email" handleChange={handleChange} value={email} />
-            </>
-          </Grid>
-          <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
-            Update
-          </Button>
-        </form>
-      </Paper>
-    </Container>
+
+      <Container component="main" maxWidth="xs">
+        {initialState && (
+          <Paper className={classes.paper} elevation={6}>
+            <Typography component="h1" variant="h5" align='center' style={{marginBottom: '16px'}}>
+              Edit Profile
+            </Typography>
+            <form className={classes.form} onSubmit={handleSubmit}>
+              <Grid container spacing={2}>
+                <>
+                  {/* TODO: split name into first and last name (by space) */}
+                  <Input name="name" label="Name" handleChange={handleChange} autoFocus value={name}/>
+                  <Input name="email" label="Email" handleChange={handleChange} value={email} />
+                </>
+              </Grid>
+              <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+                Update
+              </Button>
+            </form>
+          </Paper>
+        )} 
+      </Container>
   );
 };
 
