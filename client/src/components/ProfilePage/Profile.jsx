d30faab8d78408779c81836552de670d88cbf650
Rabia Qureshi <rabia.q123@gmail.com>
2023-03-06 18:23:31 -0500

    Remove helper function for dispatching update action on profile page Ref #42

--- client/src/components/ProfilePage/Profile.jsx
+++ client/src/components/ProfilePage/Profile.jsx
@@ -6,10 +6,8 @@ import Post from '../Posts/Post/Post';
 import { getPostsByCreatorId } from '../../actions/posts';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { getUser, updateUserProfile } from '../../actions/user';
+import { getUser } from '../../actions/user';
 import { Link } from 'react-router-dom';
-
-
 
 
 const Profile = () => {
@@ -28,16 +26,7 @@ const Profile = () => {
   useEffect(() => {
     dispatch(getUser(id));
     dispatch(getPostsByCreatorId(id));
-    // update_user("6400c5e8dcc14a33a65f7876", "test46@gmail.com", "New Wes March 2 10:24pm")
-    // console.log(name);
   }, [id]);
-
-
-  // This is a helper function that can be used for updating a users profile
-  const update_user = (id, email, name) => {
-    dispatch (updateUserProfile (id, email, name));
-    console.log ('updated_user', user);
-  }
 
   return (
     <div>
