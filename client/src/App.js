4b4739b057fff2ec8271c15fea523313cafa683d
Rabia Qureshi <rabia.q123@gmail.com>
2023-03-22 15:27:37 -0400

    Prevent users from being able to access /posts URL endpoint on clientside when not signed in Ref #64

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import PostDetails from './components/PostDetails/PostDetails';
 import Navbar from './components/Navbar/Navbar';
@@ -14,21 +15,22 @@ import Accounts from './components/Accounts/Accounts';
 
 const App = () => {
   const user = JSON.parse(localStorage.getItem('profile'));
+  const { authData } = useSelector((state) => state.auth); // authData is a prop in the auth reducer's return value 
+  // console.log(authData)
 
   return (
     <BrowserRouter>
       <Container maxWidth="xl">
         <Navbar />
         <Switch>
-          <Route path="/" exact component={() => <Redirect to="/posts" />} />
-          <Route path="/posts" exact component={Home} />
+          <Route path="/" exact component={() => (!authData && !user ? <Redirect to="/auth" /> : <Redirect to="/posts" />)} />
+          <Route path="/posts" exact component={() => (!authData && !user ? <Redirect to="/auth" /> : <Home />)} />
           <Route path="/posts/search" exact component={Home} />
           <Route path="/posts/discover" exact component={Discover} />
           <Route path="/posts/:id" exact component={PostDetails} />
           <Route path={['/creators/:name', '/tags/:name']} component={CreatorOrTag} />
           <Route path="/user/:id" exact component={Profile} />
-          <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
-          {/* <Route path="/user/:id/edit" exact component={EditScreen} /> */}
+          <Route path="/auth" exact component={() => (!authData && !user ? <Auth /> : <Redirect to="/posts" />)} />
           <Route path="/edit" exact component={EditScreen} />
           <Route path="/accounts" exact component={Accounts} />
           <Route path="/accounts/:searchUser" exact component={Accounts}/>
