eaf44f0666acc232a3994730127122d905734e46
Rabia Qureshi <rabia.q123@gmail.com>
2023-04-02 14:33:49 -0400

    Completed US10 frontend tests Ref #80

--- client/src/__tests__/app_s4.py
+++ client/src/__tests__/app_s4.py
@@ -12,6 +12,11 @@ from time import *
 class PythonOrgSearch(unittest.TestCase):
     # IDs were retrieved from inspect tool on site
 
+    invalid_email = "teehee@teehee1.com"
+    invalid_password = "TEEHEE"
+    valid_email = "teehee@teehee.com"
+    valid_password = "teehee"
+
     # incorrect email and password
     # incorrect email and correct password
     # correct email and incorrect password
@@ -21,28 +26,138 @@ class PythonOrgSearch(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
 
-    # def test_signin_invalid_email(self):
-    #     driver = self.driver
-    #     driver.maximize_window()
-    #     driver.get("http://localhost:3000/auth")
-    #     try:
-    #         element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/main/div/h1')) # Sign in form title
-    #         WebDriverWait(driver, 5).until(element_present)
-    #     except TimeoutException:
-    #         print 
-    #         "Timed out waiting for page to load"
-    #     email = driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/div[1]/div[1]/div/div/input')
-    #     password = driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/div[1]/div[2]/div/div/input')
-    #     email.send_keys("rabiatest@test.com")
-    #     password.send_keys("rabia123")
-    #     driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/button[1]').click()
-    #     try:
-    #         element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/header/div/div/button')) # Logout button
-    #         WebDriverWait(driver, 5).until(element_present)
-    #         self.assertIn("Logout", driver.page_source)
-    #     except TimeoutException:
-    #         print 
-    #         "Timed out waiting for page to load"
+    # US10: correct email and correct password
+    def test_signin_valid_credentials(self):
+        driver = self.driver
+        driver.maximize_window()
+        driver.get("http://localhost:3000/auth")
+        try:
+            element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/main/div/h1')) # Sign in form title
+            WebDriverWait(driver, 5).until(element_present)
+        except TimeoutException:
+            print 
+            "Timed out waiting for page to load"
+        email = driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/div[1]/div[1]/div/div/input')
+        password = driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/div[1]/div[2]/div/div/input')
+        email.send_keys(self.valid_email)
+        password.send_keys(self.valid_password)
+        driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/button[1]').click()
+        try:
+            element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/header/div/div/button')) # Logout button
+            WebDriverWait(driver, 5).until(element_present)
+            self.assertIn("Logout", driver.page_source)
+        except TimeoutException:
+            print 
+            "Timed out waiting for page to load"
+
+    # US10: incorrect email and incorrect password
+    def test_signin_invalid_credentials(self):
+        driver = self.driver
+        driver.maximize_window()
+        driver.get("http://localhost:3000/auth")
+        try:
+            element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/main/div/h1')) # Sign in form title
+            WebDriverWait(driver, 5).until(element_present)
+        except TimeoutException:
+            print 
+            "Timed out waiting for page to load"
+        email = driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/div[1]/div[1]/div/div/input')
+        password = driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/div[1]/div[2]/div/div/input')
+        email.send_keys(self.invalid_email)
+        password.send_keys(self.invalid_password)
+        driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/button[1]').click()
+        try:
+            element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/main/div/form/div[1]/span')) # error message
+            WebDriverWait(driver, 5).until(element_present)
+            self.assertIn("Invalid username or password.", driver.page_source)
+        except TimeoutException:
+            print 
+            "Timed out waiting for page to load"
+
+    # US10: incorrect email and correct password
+    def test_signin_invalid_email(self):
+        driver = self.driver
+        driver.maximize_window()
+        driver.get("http://localhost:3000/auth")
+        try:
+            element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/main/div/h1')) # Sign in form title
+            WebDriverWait(driver, 5).until(element_present)
+        except TimeoutException:
+            print 
+            "Timed out waiting for page to load"
+        email = driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/div[1]/div[1]/div/div/input')
+        password = driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/div[1]/div[2]/div/div/input')
+        email.send_keys(self.invalid_email)
+        password.send_keys(self.valid_password)
+        driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/button[1]').click()
+        try:
+            element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/main/div/form/div[1]/span')) # error message
+            WebDriverWait(driver, 5).until(element_present)
+            self.assertIn("Invalid username or password.", driver.page_source)
+        except TimeoutException:
+            print 
+            "Timed out waiting for page to load"
+
+    # US10: correct email and incorrect password
+    def test_signin_invalid_password(self):
+        driver = self.driver
+        driver.maximize_window()
+        driver.get("http://localhost:3000/auth")
+        try:
+            element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/main/div/h1')) # Sign in form title
+            WebDriverWait(driver, 5).until(element_present)
+        except TimeoutException:
+            print 
+            "Timed out waiting for page to load"
+        email = driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/div[1]/div[1]/div/div/input')
+        password = driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/div[1]/div[2]/div/div/input')
+        email.send_keys(self.valid_email)
+        password.send_keys(self.invalid_password)
+        driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/button[1]').click()
+        try:
+            element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/main/div/form/div[1]/span')) # error message
+            WebDriverWait(driver, 5).until(element_present)
+            self.assertIn("Invalid username or password.", driver.page_source)
+        except TimeoutException:
+            print 
+            "Timed out waiting for page to load"
+
+    # US10: correct email and correct password after a previous invalid attempt
+    def test_signin_after_invalid_attempts(self):
+        driver = self.driver
+        driver.maximize_window()
+        driver.get("http://localhost:3000/auth")
+        try:
+            element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/main/div/h1')) # Sign in form title
+            WebDriverWait(driver, 5).until(element_present)
+        except TimeoutException:
+            print 
+            "Timed out waiting for page to load"
+        email = driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/div[1]/div[1]/div/div/input')
+        password = driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/div[1]/div[2]/div/div/input')
+        # attempt to sign in with invalid credentials
+        email.send_keys(self.invalid_email)
+        password.send_keys(self.valid_password)
+        driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/button[1]').click()
+        try:
+            error_present = EC.text_to_be_present_in_element((By.XPATH, '//*[@id="root"]/div/main/div/form/div[1]/span'), "Invalid username or password.") # error message
+            WebDriverWait(driver, 5).until(error_present)
+        except TimeoutException:
+            print 
+            "Timed out waiting for page to load"
+        # attempt to sign in with valid credentials
+        email = driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/div[1]/div[1]/div/div/input')
+        password = driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/div[1]/div[2]/div/div/input')
+        email.send_keys(self.valid_email)
+        password.send_keys(self.valid_password)
+        driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/button/span[1]').click()
+        try:
+            element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/header/div/div/button')) # Logout button
+            WebDriverWait(driver, 5).until(element_present)
+            self.assertIn("Logout", driver.page_source)
+        except TimeoutException:
+            print 
+            "Timed out waiting for page to load"
 
 
     def tearDown(self):
