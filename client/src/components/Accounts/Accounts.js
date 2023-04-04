e8139b6353bdc80977b0fd1d5d410f7f07605120
Rabia Qureshi <rabia.q123@gmail.com>
2023-04-04 15:25:26 -0400

    Removed unused code from Accounts component

--- client/src/components/Accounts/Accounts.js
+++ client/src/components/Accounts/Accounts.js
@@ -1,27 +1,18 @@
 import { useSelector } from 'react-redux';
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Link } from "react-router-dom";
-import { useParams } from 'react-router-dom';
 import { useHistory, useLocation } from 'react-router-dom';
-import { Typography, Container, Grow, Grid, AppBar, TextField, Button, Paper, CircularProgress} from '@material-ui/core';
+import { Typography, Container, Grow, Grid, AppBar, TextField, Button, CircularProgress} from '@material-ui/core';
 import { ArrowBack } from '@material-ui/icons';
 import { useDispatch } from 'react-redux';
 
 import { getUsers } from '../../actions/user';
-import { getUserByName } from '../../actions/secondaryuser';
 import Account from './Account/Account';
 import Form from '../Form/Form';
 import useStyles from './styles';
 
-function getDisplayUsers (searchUser, users, secondaryuser) {
-  // console.log(searchUser, secondaryuser)
-  if (!searchUser) {
-    // console.log("LOCAL STORAGE SEARCH", localStorage.getItem("search"))
-    return orderedSearch(localStorage.getItem("search"), users);
-  } else {
-    // console.log("ORDERED SEARCH", orderedSearch(searchUser, users))
-    return orderedSearch(searchUser, users)
-  }
+function getDisplayUsers (users) {
+  return orderedSearch(localStorage.getItem("search"), users);
 }
 
 function orderedSearch (search, users) {
@@ -77,9 +68,7 @@ function orderedSearch (search, users) {
 
 
 const Accounts = () => {
-  const { searchUser } = useParams();
   const { users, isLoading } = useSelector((state) => state.user); // state.user because user is the name of the reducer
-  const { secondaryuser } = useSelector((state) => state.secondaryuser);
   const classes = useStyles();
   const dispatch = useDispatch();
   const [search, setSearch] = useState('');
@@ -87,14 +76,13 @@ const Accounts = () => {
   const history = useHistory();
   const location = useLocation();
   const sendSearch = localStorage.getItem("search") + " ";
-  var displayUsers = getDisplayUsers(searchUser, users, secondaryuser);
+  var displayUsers = getDisplayUsers(users);
 
   // console.log("DISPLAY USERS: ", displayUsers)
   // console.log("SEARCH ITEM: ", localStorage.getItem("search"))
 
   useEffect(() => {
     dispatch(getUsers());
-    dispatch(getUserByName(sendSearch));
   }, [sendSearch]);
 
   // console.log("users", users);
