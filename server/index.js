b569a9daea6b5ef20da6a0939ca89452bb8c00cf
Rabia Qureshi <rabia@rabias-mbp.home>
2024-01-24 18:53:56 -0500

    fix: add env file

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
 
+dotenv.config();
 const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }))
@@ -21,7 +23,7 @@ app.get('/', (req, res) => {
 })
 
 // now that the database is our personal one network security needs to be set up for the application (allow the ip)
-const CONNECTION_URL = 'mongodb+srv://group8:Ashisstupid23@cluster0.lvg4p5o.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5500;
 
 // Error check to ensure database is connected to the server
